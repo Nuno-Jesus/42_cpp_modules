a21193f865ef3321bd93437247e76a21364b6d69
Nuno Jesus <ncarvalh@student.42porto.com>
2023-05-21 08:43:25 +0100

    CPP02: finished exercise 00.

---
+++ CPP02/ex00/sources/Fixed.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 08:30:08 by ncarvalh          #+#    #+#             */
+/*   Updated: 2023/05/21 08:40:57 by ncarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed :: Fixed()
+{
+	std::cout << "Default constructor called." << std::endl;
+	this->value = 0;
+}
+
+Fixed :: Fixed(Fixed& num)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	*this = num;
+}
+
+Fixed :: ~Fixed()
+{
+	std::cout << "Destructor called." << std::endl;
+}
+
+Fixed& Fixed :: operator=(Fixed& right)
+{
+	std::cout << "Copy assignment operator called." << std::endl;
+	this->value = right.getRawBits();
+	return (*this);
+}
+ 
+int Fixed :: getRawBits(void) const
+{
+	std::cout << "getRawBits member function called." << std::endl;
+	return (this->value);
+}
+ 
+void Fixed :: setRawBits(int const raw)
+{
+	this->value = raw;
+}
---
+++ CPP02/ex00/sources/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 08:29:16 by ncarvalh          #+#    #+#             */
+/*   Updated: 2023/05/21 08:34:52 by ncarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
