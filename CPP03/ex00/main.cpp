8553aba38138c6e816cbafb088c1beb0ce6ecd55
Nuno Jesus <ncarvalh@student.42porto.com>
2023-05-27 18:51:09 +0100

    CPP03: removing subfolders of first exercise. Added some tests.

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -26,13 +26,19 @@ int main(int argc, char **argv)
 	ClapTrap t2(argv[1]);
 	ClapTrap t3(t2);
 
-	std::cout << "-------- Testing t3(t2) --------" << std::endl;
-	t3.attack("something");
+	std::cout << "\n-------- Testing t3(t2) --------" << std::endl;
+	t3.attack("John Cena");
+	std::cout << t3 << std::endl;
 	t3.takeDamage(9);
+	std::cout << t3 << std::endl;
+	t3.beRepaired(5);
+	std::cout << t3 << std::endl;
+	t3.takeDamage(7);
+	std::cout << t3 << std::endl;
 	t3.beRepaired(5);
 	std::cout << t3 << std::endl;
 
-	std::cout << "-------- Testing t1 = t3 --------" << std::endl;
+	std::cout << "\n-------- Testing t1 = t3 --------\n" << std::endl;
 	t1 = t3;
 	std::cout << t1 << std::endl;
 }
