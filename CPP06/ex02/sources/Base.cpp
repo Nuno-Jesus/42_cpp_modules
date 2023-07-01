8b6f7334fac25e29d10bab6157aaf311bf75ca83
Nuno Jesus <ncarvalh@student.42porto.com>
2023-07-01 16:45:25 +0100

    CPP06: added tests for bad casting.

--- CPP06/ex02/sources/Base.cpp
+++ CPP06/ex02/sources/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 16:03:19 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/07/01 16:28:09 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/07/01 16:44:57 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,15 @@ void identify(Base* p)
 	else if (dynamic_cast<C*>(p))
 		std::cout << "Identified C*\n";
 	else
-		std::cout << "Unknonw type\n";
+		std::cout << "Unknown pointer\n";
 }
 
 void identify(Base& p)
 {
 	try
 	{
-		A& a = dynamic_cast<A&>(p);
-		std::cout << "Identified A& (" << &a << ")\n";
+		(void)dynamic_cast<A&>(p);
+		std::cout << "Identified A&\n";
 		return ;
 	}
 	catch(const std::exception& e)
@@ -65,8 +65,8 @@ void identify(Base& p)
 	
 	try
 	{
-		B& b = dynamic_cast<B&>(p);
-		std::cout << "Identified B& (" << &b << ")\n";
+		(void)dynamic_cast<B&>(p);
+		std::cout << "Identified B&\n";
 		return ;
 	}
 	catch(const std::exception& e)
@@ -76,13 +76,13 @@ void identify(Base& p)
 	
 	try
 	{
-		C& c = dynamic_cast<C&>(p);
-		std::cout << "Identified C& (" << &c << ")\n";
+		(void)dynamic_cast<C&>(p);
+		std::cout << "Identified C&\n";
 		return ;
 	}
 	catch(const std::exception& e)
 	{
-		std::cout << "Unknown type casting";
+		std::cout << "Unknown type casting\n";
 	}
 	
 }
--- CPP06/ex02/sources/main.cpp
+++ CPP06/ex02/sources/main.cpp
@@ -6,23 +6,42 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 16:07:17 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/07/01 16:27:52 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/07/01 16:44:48 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
-int main(void)
+void test_bad_identify(void)
 {
-	Base *random;
-
-	std::srand(time(NULL));
+	std::cout << "\n\t======== Testing bad identify ========\n\n";
 	
+	Base *pointer = NULL;
+	Base reference;
+	
+	identify(pointer);
+	identify(reference);
+}
+
+
+void test_cool_identify(void)
+{
+	std::cout << "\n\t======== Testing cool identify ========\n\n";
+
+	Base *random;
 	random = generate();
-	//Print the address contained in random
+
 	std::cout << "Address of random: " << random << std::endl;
 	identify(random);
 	identify(*random);
 	
 	delete random;
+}
+
+int main(void)
+{
+	std::srand(time(NULL));
+	test_cool_identify();
+	test_bad_identify();
+	return (0);
 }
