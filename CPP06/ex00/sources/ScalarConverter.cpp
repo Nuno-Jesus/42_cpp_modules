92caca844b12509de01ce84626ca0246f095654b
Nuno Jesus <ncarvalh@student.42porto.com>
2023-09-22 15:58:43 +0100

    CPP05: refactored how the code is written, removed comments.

--- CPP06/ex00/sources/ScalarConverter.cpp
+++ CPP06/ex00/sources/ScalarConverter.cpp
@@ -36,31 +36,31 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& right)
 
 void ScalarConverter::convert(const std::string &str)
 {
-	if (ScalarConverter::isChar(str))
-		ScalarConverter::convertNumber(str, str[0]);
-	else if (ScalarConverter::isInt(str))
-		ScalarConverter::convertNumber(str, std::atoi(str.c_str()));
-	else if (ScalarConverter::isFloat(str))
-		ScalarConverter::convertNumber(str, std::strtof(str.c_str(), NULL));
-	else if (ScalarConverter::isDouble(str))
-		ScalarConverter::convertNumber(str, std::strtod(str.c_str(), NULL));
-	else if (ScalarConverter::isPseudo(str))
-		ScalarConverter::convertPseudo(str);
+	if (isChar(str))
+		convertNumber(str, str[0]);
+	else if (isInt(str))
+		convertNumber(str, std::atoi(str.c_str()));
+	else if (isFloat(str))
+		convertNumber(str, std::strtof(str.c_str(), NULL));
+	else if (isDouble(str))
+		convertNumber(str, std::strtod(str.c_str(), NULL));
+	else if (isPseudo(str))
+		convertPseudo(str);
 	else
 		std::cout <<"\n\t======== " << str << " is a weird type ========\n\n";
 }
 
 void ScalarConverter::convertNumber(const std::string& input, long double number)
 {
-	ScalarConverter::convertChar(static_cast<char>(number), input);
-	ScalarConverter::convertInt(static_cast<int>(number), input);
-	ScalarConverter::convertFloat(static_cast<float>(number), input);
-	ScalarConverter::convertDouble(static_cast<double>(number), input);
+	convertChar(static_cast<char>(number), input);
+	convertInt(static_cast<int>(number), input);
+	convertFloat(static_cast<float>(number), input);
+	convertDouble(static_cast<double>(number), input);
 }
 
 void ScalarConverter::convertChar(char c, const std::string &input)
 {
-	if (ScalarConverter::overflows(input, CHAR))
+	if (overflows(input, CHAR))
 		std::cout << "char: overflow\n";
 	else if (std::isprint(c))
 		std::cout << "char: '" << c << "'\n";
@@ -70,7 +70,7 @@ void ScalarConverter::convertChar(char c, const std::string &input)
 
 void ScalarConverter::convertInt(int num, const std::string &str)
 {
-	if (ScalarConverter::overflows(str, INT))
+	if (overflows(str, INT))
 		std::cout << "int: overflow\n";
 	else
 		std::cout << "int: " << num << "\n";
@@ -78,15 +78,15 @@ void ScalarConverter::convertInt(int num, const std::string &str)
 
 void ScalarConverter::convertFloat(float num, const std::string &str)
 {
-	if (ScalarConverter::overflows(str, FLOAT))
+	if (overflows(str, FLOAT))
 		std::cout << "float: overflow\n";
 	else
-		std::cout << "float: " << std::fixed << num << "f\n";
+		std::cout << "float: " << num << "f\n";
 }
 
 void ScalarConverter::convertDouble(double num, const std::string &str)
 {
-	if (ScalarConverter::overflows(str, DOUBLE))
+	if (overflows(str, DOUBLE))
 		std::cout << "double: overflow\n";
 	else
 		std::cout << "double: " << num << "\n";
@@ -104,11 +104,15 @@ void ScalarConverter::convertPseudo(const std::string &str)
 	}
 	else
 	{
-		std::cout << "float: " << str << "\n";
-		std::cout << "double: " << str.substr(0, str.size() - 1) << "\n";
+		std::cout << "float: " << str[0] << "inff" << "\n";
+		std::cout << "double: " << str[0] << "inf" << "\n";
 	}
 }
 
+//! Note:
+// std::numeric_limits<float>::min() does not represent the minimum float value
+// but the smallest floating point positive number to be represented by a float
+// To find the lowest float value, use std::numeric_limits<float>::lowest()
 bool ScalarConverter::overflows(const std::string &str, t_type type)
 {
 	long double number;
@@ -131,10 +135,6 @@ bool ScalarConverter::overflows(const std::string &str, t_type type)
 		default:
 			return (false);
 	}
-	//! Note:
-	// std::numeric_limits<float>::min() does not represent the minimum float value
-	// but the smallest floating point positive number to be represented by a float
-	// To find the lowest float value, use std::numeric_limits<float>::lowest()
 }
 
 bool ScalarConverter::isChar(const std::string &str)
@@ -147,19 +147,12 @@ bool ScalarConverter::isInt(const std::string &str)
 	size_t signal = str.find('-');
 	size_t index = 0;
 
-	// If '-' exists and is not in the beginning, abort
 	if (signal != std::string::npos && signal != 0)
 		return (false);
-
-	// If the string starts with a '-' the search index must start at 1
 	if (signal == 0)
 		index = 1;
-
-	// If any of the characters is not a digit, the number is not an int
 	if (str.find_first_not_of(DIGITS, index) != std::string::npos)
 		return (false);
-	
-	// If '-' exists, the string must have more than just the '-'
 	return (signal == 0 ? str.size() > 1 : str.size() > 0);
 }
 
@@ -170,17 +163,16 @@ bool ScalarConverter::isFloat(const std::string &str)
 	size_t dot = str.find('.');
 	size_t signal = str.find('-');
 
-	//If either the dot or the f is not found, the number is not a float
 	if (dot == std::string::npos || f == std::string::npos)
 		return (false);
 	if (signal != std::string::npos)
 		index = 1;
 
-	//Get a substring for both decimal and fractional part
 	std::string decimalPart = str.substr(index, dot - index);
 	std::string fractionalPart = str.substr(dot + 1, f - dot - 1);
 
-	//The decimal and fractional part must have at least one digit
+	if (fractionalPart.find('.') != std::string::npos)
+		return (false);
 	return (decimalPart.size() > 0 && fractionalPart.size() > 0);
 }
 
@@ -190,17 +182,16 @@ bool ScalarConverter::isDouble(const std::string &str)
 	size_t dot = str.find('.');
 	size_t signal = str.find('-');
 
-	//If the dot is missing, the number is not a double
 	if (dot == std::string::npos)
 		return (false);
 	if (signal != std::string::npos)
 		index = 1;
 
-	//Get a substring for both decimal and fractional part
 	std::string decimalPart = str.substr(index, dot - index);
 	std::string fractionalPart = str.substr(dot + 1, std::string::npos);
 
-	//The decimal and fractional part must have at least one digit
+	if (fractionalPart.find('.') != std::string::npos)
+		return (false);
 	return (decimalPart.size() > 0 && fractionalPart.size() > 0);
 }
 
