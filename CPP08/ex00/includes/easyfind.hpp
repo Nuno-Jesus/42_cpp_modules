504fed3b34600f0a7805b2bdef5e404c0ae0abfb
Nuno Jesus <ncarvalh@student.42porto.com>
2023-09-23 09:23:27 +0100

    CPP08: replaced error value with an exception throw on exercise 00.

--- CPP08/ex00/includes/easyfind.hpp
+++ CPP08/ex00/includes/easyfind.hpp
@@ -14,12 +14,15 @@
 # define EASYFIND_HPP
 
 # include <iostream>
-# include <iomanip>
-# include <climits>
+# include <limits>
 # include <algorithm>
 # include <vector>
 # include <list>
 # include <deque>
+
+# define RED	"\033[1;31m"
+# define GREEN	"\033[1;32m"
+# define RESET	"\033[0m"
 
 template <typename T>
 int easyfind(T container, int tofind);
