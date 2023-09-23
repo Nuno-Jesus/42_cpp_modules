32d46eca8233e19c0cc02f955138ce0482f32158
Nuno Jesus <ncarvalh@student.42porto.com>
2023-09-23 12:21:39 +0100

    CPP08: refined code for testing exercise 02.

--- CPP08/ex02/includes/MutantStack.hpp
+++ CPP08/ex02/includes/MutantStack.hpp
@@ -25,8 +25,8 @@
 # define GREEN	"\033[1;32m"
 # define RESET	"\033[0m"
 
-template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>
+template <typename T, typename C = std::deque<T> >
+class MutantStack : public std::stack<T, C>
 {
 	public:
 		//! Constructors and destructor
@@ -39,12 +39,12 @@ class MutantStack : public std::stack<T, Container>
 		{
 			if (*this == right)
 				return (*this);
-			std::stack<T, Container>::operator=(this, right);
+			std::stack<T, C>::operator=(this, right);
 			return (*this);
 		}
 
 		//! Member functions
-		typedef typename Container::iterator iterator;
+		typedef typename C::iterator iterator;
 
 		iterator begin(void) { return (this->c.begin()); }
 		iterator end(void) { return (this->c.end()); }
